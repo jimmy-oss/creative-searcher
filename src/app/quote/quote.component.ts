d58c7e94404d405030ef832d6bfda7159cc8bbc3
jimmy-oss <jimmynjonge51@gmail.com>
2021-12-20 11:00:44 +0300

    styled the form with good ui/ux

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,7 +13,6 @@ export class QuoteComponent implements OnInit {
     new Quote(3,'Courage doesn’t always roar', 'Sometimes courage is the little voice at the end of the day that says ‘I’ll try again tomorrow. Mary Anne Radmacher',),
     new Quote(4,'Count your age by friends', 'Not years. Count your life by smiles, not tears.”John Lennon'),
     new Quote(5,'Laughter is timeless', 'Imagination has no age. And dreams are forever.” Walt Disney'),
-    new Quote(6,'There is only one thing that makes a dream impossible to achieve', 'The fear of failure.”Paulo Coelho'),
   ];
   toggleDetails(index:any){
     this.goals[index].showDescription = !this.goals[index].showDescription;
