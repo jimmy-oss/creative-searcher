8dd3ba23d2c8a98e7de3de30a498323488b95962
jimmy-oss <jimmynjonge51@gmail.com>
2021-12-20 16:07:06 +0300

    added some two way data binding on the form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
+import { Quote} from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  newGoal = new Quote(0,"","",new Date());
   constructor() { }
 
   ngOnInit(): void {
