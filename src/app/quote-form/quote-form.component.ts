6d3ade2d7ca1d983d3915b52871641b6cd70e8c3
jimmy-oss <jimmynjonge51@gmail.com>
2021-12-20 15:33:09 +0300

    added pipes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  newGoal = new Quote(0,"","");
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   goals:Quote[] = [
-    new Quote(1, 'Dwell on the beauty of life', 'Watch the stars, and see yourself running with them.Marcus Aurelius',),
-    new Quote(2,'Nothing is impossible', 'The word itself says ‘I’m possible’!” Audrey Hepburn'),
-    new Quote(3,'Courage doesn’t always roar', 'Sometimes courage is the little voice at the end of the day that says ‘I’ll try again tomorrow. Mary Anne Radmacher',),
-    new Quote(4,'Count your age by friends', 'Not years. Count your life by smiles, not tears.”John Lennon'),
-    new Quote(5,'Laughter is timeless', 'Imagination has no age. And dreams are forever.” Walt Disney'),
+    new Quote(1, 'Dwell on the beauty of life', 'Watch the stars, and see yourself running with them.Marcus Aurelius',new Date(2021,3,14)),
+    new Quote(2,'Nothing is impossible', 'The word itself says I’m possible!” Audrey Hepburn',new Date(2020,6,9)),
+    new Quote(3,'Courage doesn’t always roar', 'Sometimes courage is the little voice at the end of the day that says I’ll try again tomorrow. Mary Anne Radmacher',new Date(2022,1,12)),
+    new Quote(4,'Count your age by friends', 'Not years. Count your life by smiles, not tears.”John Lennon',new Date(2019,0,18)),
+    new Quote(5,'Laughter is timeless', 'Imagination has no age. And dreams are forever.” Walt Disney',new Date(2021,2,14)),
   ];
   toggleDetails(index:any){
     this.goals[index].showDescription = !this.goals[index].showDescription;
